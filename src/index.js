94dd8c34c0060a1fc53e9d2a200da16060f2c7bf
claire <github email address>
2019-02-04 16:10:11 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,66 @@
+
+ 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route,  NavLink  } from 'react-router-dom';
+// The Router (our alias for BrowserRouter) component
+//The Route component is in charge of saying: "when the url matches this specified path, render this specified component".
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import Login from './Login.js'
+import About from './About.js'
+import Home from './Home.js'
+import Navbar from './Navbar.js'
 
-ReactDOM.render(
-  <Home />,
+ 
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// };
+
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+ 
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//           <label htmlFor="password">Password</label>
+//         </div>
+//         <input type="submit" value="Login" />
+//       </form>
+//     </div>
+//   );
+// };
+
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      {/* <Route path="/" render={Home} />
+      <Route path="/about" render={About} />
+      <Route exact path="/login" render={Login} /> */}
+      
+      <Navbar />
+      <Route path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+
+    </React.Fragment>
+</Router>),
   document.getElementById('root')
 );
